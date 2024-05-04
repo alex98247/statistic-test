43007a4d57e1e13d31a4aee216a843f1b8ab6b48
lev-golofastov <l.golofastov@gmail.com>
2024-05-04 12:45:19 +0300

    Add more tests

--- stattest/test/exponentiality.py
+++ stattest/test/exponentiality.py
@@ -2,6 +2,7 @@ from stattest.test.AbstractTest import AbstractTest
 from stattest.core.distribution import expon
 import numpy as np
 import scipy.stats as scipy_stats
+import scipy.special as scipy_special
 
 from stattest.test.cache import MonteCarloCacheService
 
@@ -111,3 +112,131 @@ class KSTestExp(AbstractExponentialityTest):
         ks = max(m1, m2)
 
         return ks
+
+'''
+class AhsanullahTestExp(AbstractExponentialityTest):
+
+    @staticmethod
+    def code():
+        return 'Ahsanullah_exp'
+
+    def execute_statistic(self, rvs):
+        """
+        Statistic of the exponentiality test based on Ahsanullah characterization.
+
+        Parameters
+        ----------
+        rvs : array_like
+            Array of sample data.
+
+        Returns
+        -------
+        a : float
+            The test statistic.
+        """
+
+        n = len(rvs)
+        h = 0
+        g = 0
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if abs(rvs[i] - rvs[j]) < rvs[k]:
+                        h += 1
+                    if 2 * min(rvs[i], rvs[j]) < rvs[k]:
+                        g += 1
+        a = (h - g) / (n ** 3)
+
+        return a
+'''
+
+class AtkinsonTestExp(AbstractExponentialityTest):
+
+    @staticmethod
+    def code():
+        return 'Atkinson_exp'
+
+    def execute_statistic(self, rvs, p=0.001):
+        """
+        Atkinson test statistic for exponentiality.
+
+        Parameters
+        ----------
+        p : float
+            Statistic parameter.
+        rvs : array_like
+            Array of sample data.
+
+        Returns
+        -------
+        atk : float
+            The test statistic.
+        """
+
+        n = len(rvs)
+        y = np.mean(rvs)
+        m = np.mean(np.power(rvs, p))
+        r = (m ** (1 / p)) / y
+        atk = np.sqrt(n) * np.abs(r - scipy_special.gamma(1 + p) ** (1 / p))
+
+        return atk
+
+
+class COTestExp(AbstractExponentialityTest):
+
+    @staticmethod
+    def code():
+        return 'CO_exp'
+
+    def execute_statistic(self, rvs):
+        """
+        Cox and Oakes test statistic for exponentiality.
+
+        Parameters
+        ----------
+        rvs : array_like
+            Array of sample data.
+
+        Returns
+        -------
+        co : float
+            The test statistic.
+        """
+
+        n = len(rvs)
+        y = rvs / np.mean(rvs)
+        y = np.log(y) * (1 - y)
+        co = np.sum(y) + n
+
+        return co
+
+
+class CVMTestExp(AbstractExponentialityTest):
+
+    @staticmethod
+    def code():
+        return 'CVM_exp'
+
+    def execute_statistic(self, rvs):
+        """
+        Cramer-von Mises test statistic for exponentiality.
+
+        Parameters
+        ----------
+        rvs : array_like
+            Array of sample data.
+
+        Returns
+        -------
+        cvm : float
+            The test statistic.
+        """
+
+        n = len(rvs)
+        y = rvs / np.mean(rvs)
+        z = np.sort(1 - np.exp(-y))
+        c = (2 * np.arange(1, n + 1) - 1) / (2 * n)
+        z = (z - c) ** 2
+        cvm = 1 / (12 * n) + np.sum(z)
+
+        return cvm
